dbdb207b6d220fd6a4c3ad1111dcf882f12689b3
Tachi <zaveruha007@gmail.com>
2015-06-11 16:52:54 +0300

    JS

--- js/script.js
+++ js/script.js
@@ -6,18 +6,23 @@ $(document).ready(function(){
 	var positions = new Array();
 	
 	$('#slides .slide').each(function(i){
+		//Get Slider Width
 		$positions[i] = totlaWidth;
 		totalWidth += $(this).width();
 		
+		//Check width
 		if(!$(this).width()){
 			alert('Please add a width to your images');
 			return false;
 		}
 	});
 	
+	//Set Width
 	$('#slides').width(totalWidth);
 	
 	$('#menu ul li a').click(function(e, keepScroll){
+		//Remove Active Class
 		$('li.product').removeClass('active').addClass('inactive');
+		//Add Active Class to the upper sibling
 	});
 });
